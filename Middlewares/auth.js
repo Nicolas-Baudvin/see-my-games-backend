df3a5f1de3b78ef8c4cbbc798fb2a1492eb1be18
Nicolas <nicolas.baudvin@gmail.com>
2020-01-21 16:39:03 +0100

    fix module name

--- Middlewares/auth.js
+++ Middlewares/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-modules.exports = (req, res, next) => {
+module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
         const decode = jwt.verify(token, 'RANDOM_SECRET_KEY');
