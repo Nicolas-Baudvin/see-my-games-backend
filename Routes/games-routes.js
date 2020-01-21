06462049e1622c1896dd058238a47b5309b9e4ec
Nicolas <nicolas.baudvin@gmail.com>
2020-01-21 16:39:17 +0100

    update import

--- Routes/games-routes.js
+++ Routes/games-routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const gameCtrl = require('../Controllers/games');
+const auth = require('../Middlewares/auth');
 
-router.post('/add/', gameCtrl.add);
-router.delete('/delete/:id', gameCtrl.delete);
-router.put('/update/:id', gameCtrl.update);
-router.get('/search/', gameCtrl.search);
-router.get('/search/:id', gameCtrl.searchOne);
+router.post('/add/', auth, gameCtrl.add);
+router.delete('/delete/:id', auth, gameCtrl.delete);
+router.put('/update/:id', auth, gameCtrl.update);
+router.get('/search/', auth, gameCtrl.search);
+router.get('/search/:id', auth, gameCtrl.searchOne);
 
 module.exports = router;
