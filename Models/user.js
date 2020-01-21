46c2509044de9a80ab2e42e2bf48a12299476ebb
Nicolas <nicolas.baudvin@gmail.com>
2020-01-21 12:57:15 +0100

    update user model for steam account linking

--- Models/user.js
+++ Models/user.js
@@ -3,10 +3,17 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const userShema = mongoose.Schema({
-    username: {type: String, required: true, unique: true,},
-    email: {type: String, required: true, unique: true,},
-    password: {type: String, required: true,},
-    steamid: {type: String, required: false,},
+    username: { type: String, required: true, unique: true, },
+    email: { type: String, required: true, unique: true, },
+    password: { type: String, required: true, },
+    steam_id: { type: String, required: false, },
+    steam_username: { type: String, required: false },
+    steam_profileurl: { type: String, required: false },
+    steam_avatar: { type: String, required: false },
+    steam_avatarmedium: { type: String, required: false },
+    steam_avatarfull: { type: String, required: false },
+    steam_realname: { type: String, required: false },
+    steam_timecreated: { type: Number, required: false },
 });
 
 userShema.plugin(uniqueValidator);
