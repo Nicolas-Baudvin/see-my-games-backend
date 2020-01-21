e967e0409fd013beae7347def97a80ff5e1a773d
Nicolas <nicolas.baudvin@gmail.com>
2020-01-21 14:47:49 +0100

    routes for changing password & email added

--- Routes/routes-auth.js
+++ Routes/routes-auth.js
@@ -7,5 +7,9 @@ router.post('/signup/', userCtrl.signup);
 router.post('/login/', userCtrl.login);
 router.post('/delete/', userCtrl.delete);
 router.post('/update/', userCtrl.update);
+router.post('/change-email/', userCtrl.mail_update);
+router.post('/change-password/', userCtrl.pass_update);
+router.get('/confirm-email/:token', userCtrl.confirm_mail_change);
+router.get('/confirm-password/:token', userCtrl.confirm_password_change);
 
 module.exports = router;
