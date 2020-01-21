42cb4d7b70701456fa857881447f1fb31c53301b
Nicolas <nicolas.baudvin@gmail.com>
2020-01-21 12:57:34 +0100

    update game model for steam games

--- Models/game.js
+++ Models/game.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 
 const gameShema = mongoose.Schema({
     name: { type: String, required: true },
-    release_date: { type: String, required: true },
-    imageURI: { type: String, required: true },
-    desc: { type: String, required: true },
-    ownerId: { type: Number, required: true },
+    header_img: { type: String, required: false },
+    playtime_forever: { type: Number, required: false },
+    ownerId: { type: String, required: false },
+    appid: { type: Number, required: false },
+    desc: { type: String, required: false },
+    release_date: { type: String, required: false },
 });
 
 module.exports = mongoose.model('Game', gameShema);
