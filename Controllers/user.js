f7a0eabcf0a82c54a299d1bd8d4d8496dfa1f760
Nicolas <nicolas.baudvin@gmail.com>
2020-01-21 14:46:48 +0100

    password change email sending & verification

--- Controllers/user.js
+++ Controllers/user.js
@@ -159,6 +159,45 @@ exports.mail_update = async (req, res, next) => {
 
 exports.pass_update = (req, res, next) => {
     // send mail to confirm change
+    let token;
+
+    const user = await User.findOne({ _id: req.body.userId });
+    if (!user) {
+        return res.status(400).json({ message: "Vous n'êtes pas connecté !" });
+    }
+
+    bcrypt.hash(req.body.new_password, 10)
+        .then(hash => {
+            token = jwt.sign({ userId: user._id, new_password: hash }, process.env.TOKEN_SECRET_KEY, { expiresIn: '1h' });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ err });
+        })
+    const link = `http://localhost:5000/api/auth/confirm-password/${token}`;
+    const smtpTransport = mailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.EMAIL_SMTP,
+            pass: process.env.EMAIL_PASSWORD
+        }
+    });
+    const mail = {
+        from: process.env.EMAIL_SMTP,
+        to: user.email,
+        subject: "SMG - Confirmation de changement de mot de passe",
+        html: mailType.password_confirm(link)
+    }
+    smtpTransport.sendMail(mail, (err, result) => {
+        if (err) {
+            console.log("Erreur lors de l'envoie du mail: ", err);
+            res.status(500).json({ message: "Une erreur est survenue lors de l'envoie du mail, veuillez réessayer.", err });
+        }
+        else {
+            console.log("Mail envoyé !", result);
+            res.status(200).json({ message: "Un email de confirmation a été envoyé sur votre boite mail !" });
+        }
+    });
 }
 
 exports.confirm_mail_change = (req, res, next) => {
@@ -175,9 +214,22 @@ exports.confirm_mail_change = (req, res, next) => {
         .then(err => {
             console.log(err);
             res.status(500).json({ err });
-        })
+        });
 }
 
 exports.confirm_password_change = (req, res, next) => {
     // confirm change & update database
+    const token = req.params.token;
+    const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
+    const userId = decodedToken.userId;
+    const newPassword = decodedToken.new_password;
+
+    User.updateOne({ _id: userId }, { password: newPassword })
+        .then(() => {
+            res.render('password-changed', { clientUrl: "http://localhost:3000/mon-compte/" });
+        })
+        .then(err => {
+            console.log(err);
+            res.status(500).json({ err });
+        });
 }
