dbe9a24f33a35fbc52a59ecfe3f0707a54a8d283
Nicolas <nicolas.baudvin@gmail.com>
2020-01-22 15:36:31 +0100

    apply eslint rules

--- server.js
+++ server.js
@@ -1,61 +1,63 @@
-const http = require('http');
-const app = require('./app');
+const http = require("http");
+const app = require("./app");
 
-const normalizePort = val => {
-  const port = parseInt(val, 10);
+const normalizePort = (val) => {
+    const port = parseInt(val, 10);
 
-  if (isNaN(port)) {
-    return val;
-  }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
 };
-const port = normalizePort(process.env.PORT || '5001');
-app.set('port', port);
+const port = normalizePort(process.env.PORT || "5001");
 
-const errorHandler = error => {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges.');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use.');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
+app.set("port", port);
+
+const errorHandler = (error) => {
+    if (error.syscall !== "listen") {
+        throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === "string" ? `pipe ${ address}` : `port: ${ port}`;
+
+    switch (error.code) {
+        case "EACCES":
+            console.error(`${bind } requires elevated privileges.`);
+            process.exit(1);
+            break;
+        case "EADDRINUSE":
+            console.error(`${bind } is already in use.`);
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
 };
 
 const server = http.createServer(app);
 
-server.on('error', errorHandler);
-server.on('listening', () => {
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  console.log('Listening on ' + bind);
+server.on("error", errorHandler);
+server.on("listening", () => {
+    const address = server.address();
+    const bind = typeof address === "string" ? `pipe ${ address}` : `port ${ port}`;
+
+    console.log(`Listening on ${ bind}`);
 });
 
 // socket io
-const io = require('socket.io').listen(server);
+const io = require("socket.io").listen(server);
 
-io.sockets.on('connection', (socket) => {
-  console.log("quelqu'un se connecte");
-  socket.emit('welcome_message', "Bienvenue se le chat");
-  socket.on('exchange_message', (data) => {
-    console.log(data);
-    // TODO: Logique d'échange de message / système de messagerie privée / système de channel
-  })
-} )
-
+io.sockets.on("connection", (socket) => {
+    console.log("quelqu'un se connecte");
+    socket.emit("welcome_message", "Bienvenue se le chat");
+    socket.on("exchange_message", (data) => {
+        console.log(data);
+        // TODO: Logique d'échange de message / système de messagerie privée / système de channel
+    });
+});
 
 
 server.listen(port);
