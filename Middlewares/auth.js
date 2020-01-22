ad035b894cd9261c71022b1281092938094b26f0
Nicolas <nicolas.baudvin@gmail.com>
2020-01-22 12:07:29 +0100

    minor fix

--- Middlewares/auth.js
+++ Middlewares/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
-        const decode = jwt.verify(token, 'RANDOM_SECRET_KEY');
+        const decode = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
         const userId = decode.userId;
         if (req.body.userId && req.body.userId !== userId) {
             throw 'Userid non valable';
