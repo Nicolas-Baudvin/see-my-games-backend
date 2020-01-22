861c3b4a05117d3c0b85759e1a1e86fa464ed934
Nicolas <nicolas.baudvin@gmail.com>
2020-01-22 15:40:13 +0100

    fix route pathname

--- Routes/newsletter.js
+++ Routes/newsletter.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 router.post("/subscribe/", nwsletterCtrl.subscribe);
 router.get("/confirm-subscribtion/:token/", nwsletterCtrl.confirm_subscribe);
-router.get("/send-newsletter/", nwsletterCtrl.send_newletters);
+router.get("/send/", nwsletterCtrl.send_newletters);
 
 module.exports = router;
