8e4209796dacf0d279c5a81a811e6b98b5ae779f
Nicolas <nicolas.baudvin@gmail.com>
2020-01-27 17:57:00 +0100

    apply eslint rules

--- Routes/steam-auth-routes.js
+++ Routes/steam-auth-routes.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const passport = require('passport');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
-router.get('/steam/', passport.authenticate('steam', { session: false }), (req, res) => {
+router.get("/steam/", passport.authenticate("steam", { "session": false }), (req, res) => {
     console.log(req);
 });
 
-router.get('/steam/return', passport.authenticate('steam', { session: false }), (req, res) => {
-    console.log('requêtes retournée :', req);
-const token = jwt.sign({ user: req.user }, process.env.STEAM_TOKEN_KEY, {expiresIn: '24h'});
-res.render('authenticated', { jwtToken: token, clientUrl: "http://localhost:3000" });
+router.get("/steam/return", passport.authenticate("steam", { "session": false }), (req, res) => {
+    console.log("requêtes retournée :", req.user);
+    const steamid = req.user.steamid;
+
+    res.render("authenticated", { "steamData": steamid, "clientUrl": "http://localhost:3000" });
 });
 
 module.exports = router;
--- Routes/steam-data-routes.js
+++ Routes/steam-data-routes.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const steamDataRouter = require('../Controllers/steam-data');
+const express = require("express");
+const steamDataRouter = require("../Controllers/steam-data");
 
 
 const router = express.Router();
 
-router.get('/games/:steamid/:ownerId', steamDataRouter.games);
-router.get('/summaries/:steamid/:ownerId', steamDataRouter.summaries);
-router.get('/game/:appid', steamDataRouter.game);
+router.get("/games/:steamid/:ownerId", steamDataRouter.games);
+router.get("/summaries/:steamid/:ownerId", steamDataRouter.summaries);
+router.get("/game/:appid", steamDataRouter.game);
 
 module.exports = router;
