7cd0e431d08e3898c91e183e0bf6a8839a1e5616
Nicolas <nicolas.baudvin@gmail.com>
2020-01-28 16:40:23 +0100

    apply eslint rules

--- Routes/routes-auth.js
+++ Routes/routes-auth.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
-const userCtrl = require('../Controllers/user');
+const userCtrl = require("../Controllers/user");
 
-router.post('/signup/', userCtrl.signup);
-router.post('/login/', userCtrl.login);
-router.post('/delete/', userCtrl.delete);
-router.post('/update/', userCtrl.update);
-router.post('/change-email/', userCtrl.mail_update);
-router.post('/change-password/', userCtrl.pass_update);
-router.get('/confirm-email/:token', userCtrl.confirm_mail_change);
-router.get('/confirm-password/:token', userCtrl.confirm_password_change);
+router.post("/signup/", userCtrl.signup);
+router.post("/login/", userCtrl.login);
+router.delete("/delete/", userCtrl.delete);
+router.post("/update/", userCtrl.update);
+router.post("/change-email/", userCtrl.mail_update);
+router.post("/change-password/", userCtrl.pass_update);
+router.get("/confirm-email/:token", userCtrl.confirm_mail_change);
+router.get("/confirm-password/:token", userCtrl.confirm_password_change);
 
 module.exports = router;
