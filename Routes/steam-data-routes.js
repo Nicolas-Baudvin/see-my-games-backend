297bf01d7deabfc97dbf40f0e2a21e925c274bb2
Nicolas <nicolas.baudvin@gmail.com>
2020-02-03 14:57:05 +0100

    one route added to retrive recents played games

--- Routes/steam-data-routes.js
+++ Routes/steam-data-routes.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get("/games/:steamid/:ownerId", steamDataRouter.games);
 router.get("/summaries/:steamid/:ownerId", steamDataRouter.summaries);
 router.get("/game/:appid", steamDataRouter.game);
+router.get("/recent-games/:steamid", steamDataRouter.recents_game);
 
 module.exports = router;
