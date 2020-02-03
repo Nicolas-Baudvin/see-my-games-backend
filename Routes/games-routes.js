3b446634037e4947a84bad367c8da6359bb6699b
Nicolas <nicolas.baudvin@gmail.com>
2020-02-03 14:56:16 +0100

    minors syntax fixes

--- Routes/games-routes.js
+++ Routes/games-routes.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const gameCtrl = require('../Controllers/games');
-const auth = require('../Middlewares/auth');
+const gameCtrl = require("../Controllers/games");
+const auth = require("../Middlewares/auth");
 
-router.post('/add/', auth, gameCtrl.add);
-router.delete('/delete/:id', auth, gameCtrl.delete);
-router.put('/update/:id', auth, gameCtrl.update);
-router.get('/search/', auth, gameCtrl.search);
-router.get('/search/:id', auth, gameCtrl.searchOne);
+router.post("/add/", auth, gameCtrl.add);
+router.delete("/delete/:id", auth, gameCtrl.delete);
+router.put("/update/:id", auth, gameCtrl.update);
+router.get("/search/:ownerId/", auth, gameCtrl.search);
+router.get("/search/:id/:ownerId", auth, gameCtrl.searchOne);
 
 module.exports = router;
