5f14e3316ec16d6495c48cfa6191e66663b8f175
Nicolas <nicolas.baudvin@gmail.com>
2020-02-03 14:55:35 +0100

    update Game model

--- Models/game.js
+++ Models/game.js
@@ -1,14 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const gameShema = mongoose.Schema({
-    name: { type: String, required: true },
-    header_img: { type: String, required: false },
-    playtime_forever: { type: Number, required: false },
-    ownerId: { type: String, required: false },
-    appid: { type: Number, required: false },
-    desc: { type: String, required: false },
-    release_date: { type: String, required: false },
-    hand_added: { type: Boolean, required: false }
+    "name": { "type": String, "required": true },
+    "header_img": { "type": String, "required": false },
+    "playtime_forever": { "type": Number, "required": false },
+    "ownerId": { "type": String, "required": false },
+    "appid": { "type": Number, "required": false },
+    "desc": { "type": String, "required": false },
+    "release_date": { "type": String, "required": false },
+    "hand_added": { "type": Boolean, "required": false },
+    "platform": { "type": String, "required": true }
 });
 
-module.exports = mongoose.model('Game', gameShema);
+module.exports = mongoose.model("Game", gameShema);
