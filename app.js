705c12af817cb47f2a5f29b8ef39d6794675b8d8
Nicolas <nicolas.baudvin@gmail.com>
2020-02-24 16:52:07 +0100

    fix cors origin

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ mongoose.connect(process.env.MONGO_CONNECTION_LINK,
     .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    console.log(req.headers.origin);
+    res.setHeader("Access-Control-Allow-Origin", req.headers.origin === "https://www.seemygames.fr" ? "https://www.seemygames.fr" : "https://seemygames.fr");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
     next();
