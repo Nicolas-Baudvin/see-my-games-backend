90c26fd551c1c08c5983b2e24e32f8c2b0ae1218
Nicolas <nicolas.baudvin@gmail.com>
2020-02-24 16:46:25 +0100

    add game data typing

--- Models/message.js
+++ Models/message.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const messageSchema = mongoose.Schema({
-    "message": { "type": String, "isRequired": true },
+    "message": { "type": String, "isRequired": false },
     "user": { "type": String, "isRequired": true },
     "time": { "type": String, "isRequired": true },
     "avatar": { "type": String, "isRequired": false },
@@ -9,7 +9,9 @@ const messageSchema = mongoose.Schema({
     "socketId": { "type": String, "isRequired": true },
     "isPrivate": { "type": Boolean, "isRequired": true },
     "to": { "type": String, "isRequired": false },
-    "toUsername": { "type": String, "isRequired": false }
+    "toUsername": { "type": String, "isRequired": false },
+    "game": { "type": Object, "isRequired": false },
+    "from": { "type": String, "isRequired": false }
 });
 
 module.exports = mongoose.model("message", messageSchema);
