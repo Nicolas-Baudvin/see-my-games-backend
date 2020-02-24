7f2ca19fc8b367172e2112c741e775f41512968d
Nicolas <nicolas.baudvin@gmail.com>
2020-02-24 16:46:13 +0100

    add a controller for sending steam game to chat

--- Controllers/socket.js
+++ Controllers/socket.js
@@ -1,6 +1,12 @@
 const Message = require("../Models/message");
 let counter = 0;
 
+/**
+ * @description Enregistre le message dans la BDD et l'envoie aux destinaires sur le channel correspondant
+ *
+ * @param {object} message Objet message {message: string, avatar: string, user: string, chan: string }
+ * @returns Retourne une erreur si le message est vide
+ */
 exports.sendMessage = (message, socket, namespace) => {
     if (!message.message || message.message === null) {
         return socket.emit("error_message", "Le message est vide");
@@ -20,7 +26,7 @@ exports.sendMessage = (message, socket, namespace) => {
         "socketId": socket.id,
         "isPrivate": false
     });
-    const messageToSend = { ...message, "date": `${h}h ${m}m ${s}s`, "_id": newMessage._id, "socketId": socket.id };
+    const messageToSend = { ...message, "time": `${h}h ${m}m ${s}s`, "_id": newMessage._id, "socketId": socket.id };
 
     console.log(newMessage);
 
@@ -36,6 +42,10 @@ exports.sendMessage = (message, socket, namespace) => {
         });
 };
 
+/**
+ * @description Gère l'envoie des messages privés
+ * @param {object} message => Même paramètre message que la fonction sendMessage, avec le socket et le pseudo du destinataire et de l'expéditeur
+ */
 exports.sendPrivateMessage = (message, io, socket) => {
     const date = new Date();
     const h = date.getHours();
@@ -44,5 +54,31 @@ exports.sendPrivateMessage = (message, io, socket) => {
 
     io.to(`${message.socketId}`).emit("private", { ...message, "date": `${h}h ${m}m ${s}s`, "fromSocketId": socket.id, "test": socket.conn.id, "id": counter });
     counter++;
-    // socket.to(``)
+};
+
+exports.sendSteamGame = (data, namespace, socket) => {
+    const date = new Date();
+    const h = date.getHours();
+    const m = date.getMinutes();
+    const s = date.getSeconds();
+
+    const newMessage = new Message({
+        "game": data.game,
+        "socketId": socket.id,
+        "from": data.from,
+        "isPrivate": false,
+        "chan": data.chan,
+        "user": data.from,
+        "avatar": data.avatar,
+        "time": `${h}h ${m}m ${s}s `
+    });
+
+    newMessage.save()
+        .then(() => {
+            namespace.emit("exchange_steamgame", { ...data, "socketId": socket.id, "date": `${h}h ${m}m ${s}s `, "_id": newMessage._id});
+        })
+        .catch((err) => {
+            socket.emit("error_message", "Une erreur est survenue lors de l'envoie du message", err);
+        });
+
 };
